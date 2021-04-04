f6716151d0b41f0de8a34e69777e5c11d7e6c304
kaleakash <akash300383@gmail.com>
2021-04-04 22:57:07 +0530

    Added

--- Java Program/Demo.java
+++ Java Program/Demo.java
@@ -1,5 +1,24 @@
 class Demo {
 	public static void main(String args[]) {
-		System.out.println("Welcome to Java...");
+	int abc[]=new int[10];
+	System.out.println("Size of array is "+abc.length);
+	System.out.println(abc[0]);
+	System.out.println(abc[9]);
+	abc[0]=100;
+	abc[1]=200;
+	System.out.println("Value is "+abc[0]);
+	System.out.println("Value is "+abc[1]);
+	int  temp =1000;
+	for(int i=0;i<abc.length;i++) {
+		abc[i]=temp;
+		temp = temp +100;
+	}
+	
+	System.out.println("using loop display");
+	
+	for(int a: abc) {
+		System.out.println(a);
+	}
+	//System.out.println(abc[10]);
 	}
 }
--- Java Program/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-http.listen(9999,()=>console.log("Server is running on port number"))
