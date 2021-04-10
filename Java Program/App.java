318baadf7ab200664616498c8ebed31e95d3e0df
kaleakash <akash300383@gmail.com>
2021-04-10 22:47:57 +0530

    Day 3 notes

--- Java Program/App.java
+++ Java Program/App.java
@@ -1,28 +1,26 @@
-class Car {
-	int wheel;
-	String color;
-	float price;
-
-	void start() {
-		System.out.println("Car Started...");	
-	}
-
-	void appliedGear() {
-		System.out.println("Applied Gear...");
-	}
-	void moving() {
-		System.out.println("Car is moving...");
-	}
-	void stop() {
-		System.out.println("Car Stop...");
+class Abc {
+	int a;
+	static int b;
+	void dis1() {
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
 	}
 	
 }
 class App {
 	public static void main(String args[]) {
-	Car innova = new Car();	// memory created (heap memory)
-	innova.start();
-	innova.stop();
+	Abc obj1 = new Abc();
+	Abc obj2 = new Abc();
+	
+	obj1.a=10;
+	obj1.b=20;
+	Abc.b=30;
+	
+	obj2.a=40;
+	obj2.b=50;
+	Abc.b=60;
+	obj1.dis1();	//a =10	, b=60
+	obj2.dis1();	//a=40	, b=60
 	}	
 }
 
